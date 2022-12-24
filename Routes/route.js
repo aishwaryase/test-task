524bcd2102591692fc33f6d5e23014b371ffa084
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2022-12-24 12:11:12 +0530

    done

--- Routes/route.js
+++ Routes/route.js
@@ -1,15 +1,21 @@
 const express = require("express")
-const battingController = require("../Controllers/batting")
-
+const battingController = require("../Controllers/user")
+const commnMid = require("../Middleware/Auth")
 const Router = express.Router()
 
-Router.post("/batting", battingController.createBattings)
-Router.post("/bowling", battingController.createBowlings)
-Router.post("/wicket", battingController.createWickets)
-Router.post("/postBowlings", battingController.postBowlings)
-Router.get("/getBowlings",battingController.getBowlings)
-Router.post("/postBattings", battingController.postBattings)
-Router.get("/getBattings",battingController.getBattings)
+
+Router.post("/user", battingController.createUser)
+Router.post("/userlogin", battingController.userLogin)
+Router.post("/:userId/bow_batPost",commnMid.jwtValidation,commnMid.authorization, battingController.bow_bat)
+Router.post("/:userId/batting",commnMid.jwtValidation,commnMid.authorization, battingController.createBattings)
+Router.post("/:userId/bowling",commnMid.jwtValidation,commnMid.authorization, battingController.createBowlings)
+Router.post("/:userId/wicket",commnMid.jwtValidation,commnMid.authorization, battingController.createWickets)
+Router.post("/:userId/postBowlings",commnMid.jwtValidation,commnMid.authorization, battingController.postBowlings)
+Router.get("/:userId/getBowlings",commnMid.jwtValidation,commnMid.authorization,battingController.getBowlings)
+Router.post("/:userId/postBattings",commnMid.jwtValidation,commnMid.authorization, battingController.postBattings)
+Router.get("/:userId/getBattings",commnMid.jwtValidation,commnMid.authorization,battingController.getBattings)
+Router.post("/:userId/postDrills",commnMid.jwtValidation,commnMid.authorization, battingController.createDrills)
+Router.get("/:userId/getRoutine",commnMid.jwtValidation,commnMid.authorization, battingController.getRoutine)
 
 
 //************ checking your end point valid or not */
